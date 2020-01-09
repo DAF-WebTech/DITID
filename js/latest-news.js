fa36886b63e3b8a6d5c6b9d090a1844b2336d718
PORTER David <david.porter@daf.qld.gov.au>
2020-01-09 11:00:35 +1000

    fix truncate function

--- js/latest-news.js
+++ js/latest-news.js
@@ -1,107 +1,111 @@
 var app = new Vue({
-    el: "#newslist",
-    data: {
-        news: initialNews,
-        start: 0,
-        pageSize: 6,
-        category: "all",
-        region: "all",
-        year: "all",
-        month: "-1",
-        length: 0,
-        activeButton: 1
-    },
-    computed: {
-        items : function() {
-            var category = this.category;
-            var region = this.region;
-            var year = this.year;
-            var month = this.month;
-            
-            var filtered = this.news.filter(function(n) {
-                var retVal = category === "all" || n.category === "all" || category === n.category;
-                retVal = retVal && (region === "all" || n.region === "all" || region === n.region);
-                if (year != "all") {
-                    retVal = retVal && (n.newsPublicationDate.getFullYear() == year);
-                    if (month != "-1")
-                        retVal = retVal && (n.newsPublicationDate.getMonth() == month);
-                }
-                return retVal;
-            });
-            this.length = filtered.length;
-            return filtered.slice(this.start, this.start + this.pageSize);
-        }
-    },
-    methods: {
-        page: function(event) {
-            var text = event.target.textContent;
-            if (text == "Prev") {
-                --this.activeButton;
-                this.activeButton = Math.max(1, this.activeButton);
-            }
-            else if (text == "Next") {
-                ++this.activeButton;
-                this.activeButton = Math.min(Math.ceil(this.length / this.pageSize), this.activeButton);
-            }
-            else {
-                this.activeButton =  parseInt(event.target.textContent);
-            }
-            
-            this.start = (this.activeButton - 1) * this.pageSize;
-            event.preventDefault(); 
-				},
-				truncate: function(html, length) {
-					length = (typeof length == 'undefined' ? 75 : length);/*default value is 75*/
-					var div = document.createElement("div");
-					div.innerHTML = html;
-					var retVal = div.textContent.substring(0, length);
-					retVal = retVal.substring(0, retVal.lastIndexOf(" "));
-					if (retVal.length > length)
-						retVal += "…";
-					return retVal;
+	el: "#newslist",
+	data: {
+		news: initialNews,
+		start: 0,
+		pageSize: 6,
+		category: "all",
+		region: "all",
+		year: "all",
+		month: "-1",
+		length: 0,
+		activeButton: 1
+	},
+	computed: {
+		items: function () {
+			var category = this.category;
+			var region = this.region;
+			var year = this.year;
+			var month = this.month;
+
+			var filtered = this.news.filter(function (n) {
+				var retVal = category === "all" || n.category === "all" || category === n.category;
+				retVal = retVal && (region === "all" || n.region === "all" || region === n.region);
+				if (year != "all") {
+					retVal = retVal && (n.newsPublicationDate.getFullYear() == year);
+					if (month != "-1")
+						retVal = retVal && (n.newsPublicationDate.getMonth() == month);
 				}
-    }
+				return retVal;
+			});
+			this.length = filtered.length;
+			return filtered.slice(this.start, this.start + this.pageSize);
+		}
+	},
+	methods: {
+		page: function (event) {
+			var text = event.target.textContent;
+			if (text == "Prev") {
+				--this.activeButton;
+				this.activeButton = Math.max(1, this.activeButton);
+			}
+			else if (text == "Next") {
+				++this.activeButton;
+				this.activeButton = Math.min(Math.ceil(this.length / this.pageSize), this.activeButton);
+			}
+			else {
+				this.activeButton = parseInt(event.target.textContent);
+			}
+
+			this.start = (this.activeButton - 1) * this.pageSize;
+			event.preventDefault();
+		},
+		truncate: function (html, length) {
+			length = (typeof length == 'undefined' ? 75 : length);/*default value is 75*/
+			var div = document.createElement("div");
+			div.innerHTML = html;
+			// add a space after each element in case we got adjacent elements, we don't want the text to run into each other
+			div.querySelectorAll("*").forEach(function (e) {
+				e.textContent += " ";
+			});
+			var retVal = div.textContent;
+			retVal.replace(/\s{2}/g, " ");// remove double whitespace
+			if (retVal.length > length) // truncate
+				retVal = retVal.substr(0, retVal.lastIndexOf(" ", length)) + "…";
+			return retVal;
+		}
+	}
 });
 
 
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-    document.getElementById("applyButton").addEventListener("click", function() {
-        app.start = 0;
-        app.category = document.getElementById("categorySelect").value;
-        app.region = document.getElementById("regionSelect").value;
-        app.pageSize = parseInt(document.getElementById("sizeSelect").value);
-        var dateRange = document.getElementById("monthSelect");
-        if (dateRange.value == "all") {
-            app.month = "all";
-            app.year = "all";
-        }
-        else {
-            var o = dateRange.options[dateRange.selectedIndex];
-            app.month = o.dataset.month;
-            app.year = o.dataset.year;
-        }
-        app.activeButton = 1;
-    });
+	document.getElementById("applyButton").addEventListener("click", function () {
+		app.start = 0;
+		app.category = document.getElementById("categorySelect").value;
+		app.region = document.getElementById("regionSelect").value;
+		app.pageSize = parseInt(document.getElementById("sizeSelect").value);
+		var dateRange = document.getElementById("monthSelect");
+		if (dateRange.value == "all") {
+			app.month = "all";
+			app.year = "all";
+		}
+		else {
+			var o = dateRange.options[dateRange.selectedIndex];
+			app.month = o.dataset.month;
+			app.year = o.dataset.year;
+		}
+		app.activeButton = 1;
+	});
 
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", function() {
-        var news = JSON.parse(xhr.responseText);
-        news.forEach(function(n) {
-            n.newsPublicationDate = new Date(n.newsPublicationDate);
-        });
-        app.news = news;
-    });
-    xhr.open("GET", ajaxUrl);
-    xhr.send();
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener("load", function () {
+		var news = JSON.parse(xhr.responseText);
+		news.forEach(function (n) {
+			n.newsPublicationDate = new Date(n.newsPublicationDate);
+		});
+		app.news = news;
+	});
+	xhr.open("GET", ajaxUrl);
+	xhr.send();
 
-    var options = document.querySelectorAll("select#monthSelect option");
-    options.forEach(function(o) {
-				o.dataset.month = 
-					["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-						.indexOf(o.textContent);
-    })
+	var options = document.querySelectorAll("select#monthSelect option");
+	options.forEach(function (o) {
+		o.dataset.month =
+			["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+				.indexOf(o.textContent);
+	})
 
-    
+
 });
