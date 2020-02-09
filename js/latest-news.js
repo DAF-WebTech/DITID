93dd0aa91e3f34be1a22719d0fd1e3e6fbac4548
PORTER David <david.porter@daf.qld.gov.au>
2020-02-10 09:33:01 +1000

    after many deve edits

--- js/latest-news.js
+++ js/latest-news.js
@@ -1,61 +1,67 @@
-var app = new Vue({
-	el: "#newslist",
-	data: {
-		news: initialNews,
-		start: 0,
-		pageSize: 6,
-		category: "all",
-		region: "all",
-		year: "all",
-		month: "-1",
-		length: 0,
-		activeButton: 1
-	},
-	computed: {
-		items: function () {
-			var category = this.category;
-			var region = this.region;
-			var year = this.year;
-			var month = this.month;
 
-			var filtered = this.news.filter(function (n) {
-				var retVal = category === "all" || n.category === "all" || category === n.category;
-				retVal = retVal && (region === "all" || n.region === "all" || region === n.region);
-				if (year != "all") {
-					retVal = retVal && (n.newsPublicationDate.getFullYear() == year);
-					if (month != "-1")
-						retVal = retVal && (n.newsPublicationDate.getMonth() == month);
+document.addEventListener("DOMContentLoaded", function () {
+	var app = new Vue({
+		el: "#newslist",
+		data: {
+			news: [],
+			start: 0,
+			pageSize: 6,
+			category: "all",
+			region: "all",
+			year: "all",
+			month: "-1",
+			length: 0,
+			activeButton: 1
+		},
+		computed: {
+			items: function () {
+				var category = this.category;
+				var region = this.region;
+				var year = this.year;
+				var month = this.month;
+
+				var filtered = this.news.filter(function (n) {
+
+					var retVal = category === "all" || n.categories.indexOf(category) > -1;
+
+					retVal = retVal && (region === "all" || n.regions.indexOf(region) > -1);
+
+
+					if (year != "all") {
+						retVal = retVal && (n.newsPublicationDate.getFullYear() == year);
+						if (month != "-1")
+							retVal = retVal && (n.newsPublicationDate.getMonth() == month);
+					}
+					return retVal;
+				});
+				this.length = filtered.length;
+				return filtered.slice(this.start, this.start + this.pageSize);
+			}
+		},
+		methods: {
+			page: function (event) {
+				var text = event.target.textContent;
+				if (text == "Prev") {
+					--this.activeButton;
+					this.activeButton = Math.max(1, this.activeButton);
 				}
-				return retVal;
-			});
-			this.length = filtered.length;
-			return filtered.slice(this.start, this.start + this.pageSize);
+				else if (text == "Next") {
+					++this.activeButton;
+					this.activeButton = Math.min(Math.ceil(this.length / this.pageSize), this.activeButton);
+				}
+				else {
+					this.activeButton = parseInt(event.target.textContent);
+				}
+
+				this.start = (this.activeButton - 1) * this.pageSize;
+				event.preventDefault();
+			}
 		}
-	},
-	methods: {
-		page: function (event) {
-			var text = event.target.textContent;
-			if (text == "Prev") {
-				--this.activeButton;
-				this.activeButton = Math.max(1, this.activeButton);
-			}
-			else if (text == "Next") {
-				++this.activeButton;
-				this.activeButton = Math.min(Math.ceil(this.length / this.pageSize), this.activeButton);
-			}
-			else {
-				this.activeButton = parseInt(event.target.textContent);
-			}
-
-			this.start = (this.activeButton - 1) * this.pageSize;
-			event.preventDefault();
-		}
-	}
-});
+	});
 
 
 
-window.addEventListener("load", function () {
+
 
 	document.getElementById("applyButton").addEventListener("click", function () {
 		app.start = 0;
@@ -82,15 +88,9 @@ window.addEventListener("load", function () {
 		});
 		app.news = news;
 	});
+
 	xhr.open("GET", ajaxUrl);
 	xhr.send();
-
-	var options = document.querySelectorAll("select#monthSelect option");
-	options.forEach(function (o) {
-		o.dataset.month =
-			["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-				.indexOf(o.textContent);
-	})
 
 
 });
--- js/our-work.js
+++ js/our-work.js
@@ -1,61 +1,67 @@
-var PAGE_SIZE = 6;
+document.addEventListener("DOMContentLoaded", function () {
+	var PAGE_SIZE = 12;
 
-var urlArgs = function() {
-    var args = {};
-    var query = location.search.substring(1);
-    var pairs = query.split("&");
-    pairs.forEach(function(pair) {
-        var pos = pair.indexOf("=");
-        if (pos == -1)
-            return;
-        var name = pair.substring(0, pos);
-        var value = pair.substring(pos + 1);
-        value = decodeURIComponent(value);
-        args[name] = value;
-    });
-    return args;
-}
+	var urlArgs = function () {
+		var args = {};
+		var query = location.search.substring(1);
+		var pairs = query.split("&");
+		pairs.forEach(function (pair) {
+			var pos = pair.indexOf("=");
+			if (pos == -1)
+				return;
+			var name = pair.substring(0, pos);
+			var value = pair.substring(pos + 1);
+			value = decodeURIComponent(value);
+			args[name] = value;
+		});
+		return args;
+	}
 
-var args = urlArgs();
-args.region = args.region || "all";
-args.category = args.category || "all";
-document.getElementById("categorySelect").value = args.category;
-document.getElementById("regionSelect").value = args.region;
+	var args = urlArgs();
+	args.region = args.region || "all";
+	args.category = args.category || "all";
+	document.getElementById("categorySelect").value = args.category;
+	document.getElementById("regionSelect").value = args.region;
 
-var app = new Vue({
-    el: "#projectList",
-    data: {
-        projects: JSON.parse(document.getElementById("projectJson").textContent),
-        pageSize: PAGE_SIZE,
-        activePage: 1,
-        category: args.category,
-        region: args.region,
-        length: 0
-    },
-    computed: {
-        items: function() {
-            var category = this.category;
-            var region = this.region;
-            
-            var filtered = this.projects.filter(function(p) {
-                var retVal = (category === "all" || p.category === category);
-                retVal = retVal && (region === "all" || p.region === "all" || p.region === region);
-                return retVal;
-            });
-            this.length = filtered.length;
-            return filtered.slice(0, this.pageSize);
-        }
-    },
-    methods: {
-        more: function() {
-            this.pageSize += PAGE_SIZE;
-        }
-    }
-});    
+	var projectsFeed = JSON.parse(document.getElementById("projectJson").textContent);
 
-document.getElementById("filterButton").addEventListener("click", function() {
-    app.pageSize = PAGE_SIZE;
-    app.category = document.getElementById("categorySelect").value;
-    app.region = document.getElementById("regionSelect").value;
-    app.activePage = 1;
+
+	var app = new Vue({
+		el: "#projectList",
+		data: {
+			projects: projectsFeed,
+			pageSize: PAGE_SIZE,
+			activePage: 1,
+			category: args.category,
+			region: args.region,
+			length: 0
+		},
+		computed: {
+			items: function () {
+				var category = this.category;
+				var region = this.region;
+
+				var filtered = this.projects.filter(function (p) {
+					var retVal = (category === "all" || p.categories.indexOf(category) > -1);
+					retVal = retVal && (region === "all" || p.regions.indexOf(region) > -1);
+					return retVal;
+				});
+				this.length = filtered.length;
+				return filtered.slice(0, this.pageSize);
+			}
+		},
+		methods: {
+			more: function () {
+				this.pageSize += PAGE_SIZE;
+			}
+		}
+	});
+
+	document.getElementById("filterButton").addEventListener("click", function () {
+		app.pageSize = PAGE_SIZE;
+		app.category = document.getElementById("categorySelect").value;
+		app.region = document.getElementById("regionSelect").value;
+		app.activePage = 1;
+	});
+
 });
