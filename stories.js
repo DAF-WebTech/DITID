c218cc5c4619c5faa795f5fb38f6bd616fd04bfa
PORTER David <david.porter@daf.qld.gov.au>
2020-02-10 09:34:44 +1000

    new file

---
+++ stories.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	var stories = JSON.parse(document.getElementById("storiesJson").textContent);
+
+
+    var categories = [];
+    stories.forEach(function(s) {
+        s.updated = new Date(s.updated); // fix date type
+        
+        s.categories.forEach(function(c) { // get list of unique categories
+            if (categories.indexOf(c) == -1)
+                categories.push(c);
+        });
+        
+        // create a thumbnail from the first picture we find in the content
+        var e = document.createElement("div");
+        e.innerHTML = s.content;
+        s.img = e.querySelector("img").outerHTML;
+    });
+
+    categories.sort();
+    categories.unshift("all");
+    var options = categories.map(function(c) {
+        var key = c.charAt(0).toUpperCase() + c.slice(1);
+        key = key.replace(/-/g, " ");
+        return {text: key, value: c}
+    });
+
+    var app = new Vue({
+        el: "#storyContent",
+        data: {
+            length: stories.length,
+            pageSize: 6,
+            pageNum: 1,
+            category: "all",
+            options: options
+        },
+        computed: {
+            items: function() { 
+                var filtered = [];
+                for (var i = 0; i < stories.length; ++i) {
+                    if (this.category == "all" || stories[i].categories.indexOf(this.category) > -1)
+                        filtered.push(stories[i])
+                }
+
+                this.length = filtered.length;
+                var startIndex = (this.pageNum - 1) * this.pageSize;
+                var sliced = filtered.slice(startIndex, startIndex + this.pageSize);
+                return sliced;
+            }
+        },
+        methods: {
+            page: function(event) {
+                var text = event.target.textContent;
+                if (text == "Prev") {
+                    --this.pageNum;
+                    this.pageNum = Math.max(1, this.pageNum);
+                }
+                else if (text == "Next") {
+                    ++this.pageNum;
+                    this.pageNum = Math.min(Math.ceil(this.length / this.pageSize), this.pageNum);
+                }
+                else {
+                    this.pageNum = parseInt(text);
+                }
+                
+                event.preventDefault();
+            },
+            
+            filter: function(event) {
+                this.category = document.getElementById("categorySelect").value;
+                this.pageNum = 1;
+            },
+    		truncate: function (html, length) {
+    			length = (typeof length == 'undefined' ? 75 : length);/*default value is 75*/
+    			var div = document.createElement("div");
+    			div.innerHTML = html;
+    			// add a space after each element in case we got adjacent elements, we don't want the text to run into each other
+    			div.querySelectorAll("*").forEach(function (e) {
+    				e.textContent += " ";
+    			});
+    			var retVal = div.textContent;
+    			retVal.replace(/\s{2}/g, " ");// remove double whitespace
+    			if (retVal.length > length) // truncate
+    				retVal = retVal.substr(0, retVal.lastIndexOf(" ", length)) + "…";
+    			return retVal;
+    		},
+    		formatDate: function(d) {
+    		    var a = d.substring(4).split(" ");
+    		    a[1] = a[1].replace(/^0/, "");
+    		    a[1] += ",";
+    		    return a.join(" ");
+    		}
+        }
+    });
+});    
+    
